79d72b9e4256b09930791f36d8fe4bc8dd1b13e1
ayeryn <erynli@utexas.edu>
2022-12-29 20:01:19 -0600

    Add count_bits

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -2,8 +2,16 @@ from test_framework import generic_test
 
 
 def count_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    num_of_bits = 0
+
+    while x:
+        # Gets the last bit of x
+        b = x & 1
+        num_of_bits += b
+
+        # Right shift to discard the current last bit
+        x >>= 1
+    return num_of_bits
 
 
 if __name__ == '__main__':
