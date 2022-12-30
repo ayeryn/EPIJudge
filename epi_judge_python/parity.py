6572c2a0b9094d6ef69a8c00d40ad73750b3556e
ayeryn <erynli@utexas.edu>
2022-12-29 22:19:43 -0600

    Add final parity solution and add notes

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -15,7 +15,7 @@ def parity_brute(x: int) -> int:
     return p % 2
 
 
-def parity(x: int) -> int:
+def parity_k(x: int) -> int:
     """
     x & (x-1) = x with the lowest **set** bit removed
     O(k) : k is the number of set bits
@@ -28,6 +28,24 @@ def parity(x: int) -> int:
         x &= x-1
 
     return p
+
+# def parity_cache(): break into parts and cache the first x entries
+
+
+def parity(x: int) -> int:
+    # Every round do: (higher half XOR lower half)
+    # The higher half will preserve, BUT
+    # only the lowest bit will be observed at the end
+    # See notes for detailed explanation
+    x = x ^ (x >> 32)
+    x = x ^ (x >> 16)
+    x = x ^ (x >> 8)
+    x = x ^ (x >> 4)
+    x = x ^ (x >> 2)
+    x = x ^ (x >> 1)
+
+    # Ignore leading 0s
+    return x & 0x1
 
 
 if __name__ == '__main__':
