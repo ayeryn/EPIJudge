50f0c26d68a805a7484b21056427b9f080119f5c
ayeryn <erynli@utexas.edu>
2022-12-29 20:36:12 -0600

    Add parity_brute and parity_k

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,9 +1,31 @@
 from test_framework import generic_test
 
 
-def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+def parity_brute(x: int) -> int:
+    """
+    Brute-force implementation
+    O(n)
+    """
+    p = 0
+    while x:
+        # Process each bit from lowest bit
+        if x & 1:
+            p += 1
+        x >>= 1
+    return p % 2
+
+
+def parity_k(x: int) -> int:
+    """
+    x & (x-1) = x with the lowest **set** bit removed
+    O(k) : k is the number of set bits
+    """
+    p = 0
+    while x:
+        p ^= 1
+        x &= x-1
+
+    return p
 
 
 if __name__ == '__main__':
